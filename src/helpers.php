5b97b8c413b173734dff3715278f72a03ebde6f9
sbuspl <83451866+sbuspl@users.noreply.github.com>
2024-01-17 00:05:26 +0100

    Laravel 10 mail notification update - remove value() function

--- src/helpers.php
+++ src/helpers.php
@@ -217,20 +217,6 @@ if ( ! function_exists('array_get') && ! function_exists('app'))
 	}
 }
 
-if ( ! function_exists('value'))
-{
-	/**
-	 * Return the default value of the given value.
-	 *
-	 * @param  mixed  $value
-* @return mixed
-	 */
-	function value($value)
-	{
-		return $value instanceof Closure ? $value() : $value;
-	}
-}
-
 if ( ! function_exists('array_pluck'))
 {
 	/**
